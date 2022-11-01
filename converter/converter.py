425309f4d35ce9a061835d5a97e6687aef25f2dc
funkylen <funkylen@gmail.com>
2022-11-01 14:36:41 +0300

    01.11.2022 practice

--- converter/converter.py
+++ converter/converter.py
@@ -1,61 +1,17 @@
-import prompt
-
-CM_IN_M = 100
-M_IN_KM = 1000
-
-
-def print_menu(modes):
-    print('Выбор типа конвертации')
-
-    for mode in modes:
-        index = mode.get('index')
-
-        from_type = mode.get('from_type')
-        to_type = mode.get('to_type')
-
-        print(f'{index}. {from_type} -> {to_type}')
-
-
-def get_modes():
-    return [
-        {
-            'index': 1,
-            'from_type': 'CM',
-            'to_type': 'M',
-            'handler': lambda centimeters: centimeters / CM_IN_M,
-        },
-        {
-            'index': 2,
-            'from_type': 'M',
-            'to_type': 'CM',
-            'handler': lambda meters: meters * CM_IN_M,
-        },
-        {
-            'index': 3,
-            'from_type': 'M',
-            'to_type': 'KM',
-            'handler': lambda meters: meters / M_IN_KM,
-        },
-    ]
-
-
 def convert():
-    modes = get_modes()
+    user_num = input("Введите число")
+    spisok = ["mm", "cm", "km", "dm", "m", "nm"]
+    print(spisok)
+    user_tw = input("Выберите единицу")
+    user_fr = input("Выберите единицу")
+    spisok2 = [{'единица измерения': 'm', 'множитель': 1},
+               {'единица измерения': 'mm', 'множитель': 0.001},
+               {'единица измерения': 'cm', 'множитель': 0.01},
+               {'единица измерения': 'km', 'множитель': 1000},
+               {'единица измерения': 'nm', 'множитель': 0.000000001}
+               ]
 
-    print_menu(modes)
-
-    convert_type = prompt.integer('Выберите тип: ')
-
-    number = prompt.real('Введите значение: ')
-
-    for mode in modes:
-        if convert_type == mode.get('index'):
-            converted_number = mode.get('handler')(number)
-
-            from_type = mode.get('from_type')
-            to_type = mode.get('to_type')
-
-            print(f'{number} {from_type} -> {converted_number} {to_type}')
-            return
-
-    raise 'Неизвестный тип конвертации'
+    for c in spisok2:
+        if user_tw==c['единица измерения']:
+            user_th=c['множитель']
+    print(user_th)        
