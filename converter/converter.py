4cad964676b74b6efd6b34a7edd903a85776561b
funkylen <funkylen@gmail.com>
2022-11-08 14:41:38 +0300

    new practice. done. functions

--- converter/converter.py
+++ converter/converter.py
@@ -1,17 +1,37 @@
-def convert():
-    user_num = input("Введите число")
-    spisok = ["mm", "cm", "km", "dm", "m", "nm"]
-    print(spisok)
-    user_tw = input("Выберите единицу")
-    user_fr = input("Выберите единицу")
-    spisok2 = [{'единица измерения': 'm', 'множитель': 1},
-               {'единица измерения': 'mm', 'множитель': 0.001},
-               {'единица измерения': 'cm', 'множитель': 0.01},
-               {'единица измерения': 'km', 'множитель': 1000},
-               {'единица измерения': 'nm', 'множитель': 0.000000001}
-               ]
+import prompt
 
-    for c in spisok2:
-        if user_tw==c['единица измерения']:
-            user_th=c['множитель']
-    print(user_th)        
+
+def input_unit(units):
+    while True:
+        firstunit = input("Выберите единицу: ")
+        if firstunit in units:
+            break
+    return firstunit
+
+
+def get_factor(unit_for_chek, units_table):
+    for unit in units_table:
+        if unit_for_chek == unit['единица измерения']:
+            return unit['множитель']
+
+
+def convert():
+    units_table = [{'единица измерения': 'm', 'множитель': 1},
+                   {'единица измерения': 'mm', 'множитель': 0.001},
+                   {'единица измерения': 'cm', 'множитель': 0.01},
+                   {'единица измерения': 'km', 'множитель': 1000},
+                   {'единица измерения': 'nm', 'множитель': 0.000000001}
+                   ]
+    units = [unit['единица измерения'] for unit in units_table]
+    user_num = prompt.real("Введите число: ")
+    for unit in units_table:
+        print(unit['единица измерения'])
+
+    firstunit = input_unit(units)
+    secondunit = input_unit(units)
+
+    firstfactor = get_factor(firstunit, units_table)
+    secondfactor = get_factor(secondunit, units_table)
+
+    result = float(user_num) * firstfactor / secondfactor
+    print(result)
